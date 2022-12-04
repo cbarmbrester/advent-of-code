b47140227dc664f2cbe68be1c18cdf93d8b38714
cbarmbrester <119710970+cbarmbrester@users.noreply.github.com>
2022-12-03 23:41:33 -0600

    Update day04.py
    Cleaned up code to convert to int before the if statements

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -5,13 +5,14 @@ input = list(txt.splitlines())
 
 count_1 = 0
 count_2 = 0
+
 for a in input:
     pair = a.split(',')
-    left_pair = pair[0].split('-')
-    right_pair = pair[1].split('-')
-    if((int(left_pair[0]) <= int(right_pair[0]) and int(left_pair[1]) >= int(right_pair[1])) or (int(right_pair[0]) <= int(left_pair[0]) and int(right_pair[1]) >= int(left_pair[1]))):
+    left_pair = [int(x) for x in pair[0].split('-')]
+    right_pair = [int(x) for x in pair[1].split('-')]
+    if((left_pair[0] <= right_pair[0] and left_pair[1] >= right_pair[1]) or (right_pair[0] <= left_pair[0] and right_pair[1] >= left_pair[1])):
         count_1+=1
-    if((int(left_pair[0]) >= int(right_pair[0]) and int(left_pair[0]) <= int(right_pair[1])) or (int(left_pair[1]) >= int(right_pair[0]) and int(left_pair[0]) <= int(right_pair[1])) or (int(right_pair[0]) >= int(left_pair[0]) and int(right_pair[0]) <= int(left_pair[1])) or (int(right_pair[1]) >= int(left_pair[0]) and int(right_pair[1]) <= int(left_pair[1]))):
-        count_2+=1
+    if((left_pair[0] >= right_pair[0] and left_pair[0] <= right_pair[1]) or (left_pair[1] >= right_pair[0] and left_pair[0] <= right_pair[1]) or (right_pair[0] >= left_pair[0] and right_pair[0] <= left_pair[1]) or (right_pair[1] >= left_pair[0] and right_pair[1] <= left_pair[1])):
+        count_2 +=1
 print(count_1)
 print(count_2)
