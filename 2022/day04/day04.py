2909d29e707de1114cf9b49129d6ac919e33d51b
cbarmbrester <119710970+cbarmbrester@users.noreply.github.com>
2022-12-03 23:35:58 -0600

    Update day04.py

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -5,8 +5,6 @@ input = list(txt.splitlines())
 
 count_1 = 0
 count_2 = 0
-correct = []
-incorrect = []
 for a in input:
     pair = a.split(',')
     left_pair = pair[0].split('-')
@@ -14,6 +12,6 @@ for a in input:
     if((int(left_pair[0]) <= int(right_pair[0]) and int(left_pair[1]) >= int(right_pair[1])) or (int(right_pair[0]) <= int(left_pair[0]) and int(right_pair[1]) >= int(left_pair[1]))):
         count_1+=1
     if((int(left_pair[0]) >= int(right_pair[0]) and int(left_pair[0]) <= int(right_pair[1])) or (int(left_pair[1]) >= int(right_pair[0]) and int(left_pair[0]) <= int(right_pair[1])) or (int(right_pair[0]) >= int(left_pair[0]) and int(right_pair[0]) <= int(left_pair[1])) or (int(right_pair[1]) >= int(left_pair[0]) and int(right_pair[1]) <= int(left_pair[1]))):
-        count_2 +=1
+        count_2+=1
 print(count_1)
 print(count_2)
