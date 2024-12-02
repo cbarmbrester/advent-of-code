52063a8da6777befcaecfbf16e6157627607c9c7
cbarmbrester <119710970+cbarmbrester@users.noreply.github.com>
2024-12-01 23:39:50 -0600

    Fixed initial split to parse automatically to int

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -6,12 +6,12 @@ input = list(txt.splitlines())
 total_1 = 0
 total_2 = 0
 for a in input:
-    lists = a.split(' ')
+    lists = [int(x) for x in a.split(' ')]
     flag = 0
-    if all(int(lists[i]) <= int(lists[i+1]) for i in range(len(lists)-1)) or all(int(lists[j]) >= int(lists[j+1]) for j in range(len(lists)-1)):
+    if all(lists[i] <= lists[i+1] for i in range(len(lists)-1)) or all(lists[j] >= lists[j+1] for j in range(len(lists)-1)):
         flag = 1
         for b in range(len(lists)-1):
-            if abs(int(lists[b+1]) - int(lists[b])) > 3 or abs(int(lists[b+1]) - int(lists[b])) < 1:
+            if abs(lists[b+1] - lists[b]) > 3 or abs(lists[b+1] - lists[b]) < 1:
                 flag = 0
     if flag:
         total_1 += 1
@@ -20,10 +20,10 @@ for a in input:
         for x in range(len(lists)):
             newList = lists.copy()
             newList.pop(x)
-            if all(int(newList[i]) <= int(newList[i+1]) for i in range(len(newList)-1)) or all(int(newList[j]) >= int(newList[j+1]) for j in range(len(newList)-1)):
+            if all(newList[i] <= newList[i+1] for i in range(len(newList)-1)) or all(newList[j] >= newList[j+1] for j in range(len(newList)-1)):
                 flag = 1
                 for b in range(len(newList)-1):
-                    if abs(int(newList[b+1]) - int(newList[b])) > 3 or abs(int(newList[b+1]) - int(newList[b])) < 1:
+                    if abs(newList[b+1] - newList[b]) > 3 or abs(newList[b+1] - newList[b]) < 1:
                         flag = 0
             if flag:
                 total_2 += 1
